b0688d8b44d2e951bacc7d958ba09968a91f3a92
Ajay chawariya <Ajaychawariya34@gmal.com>
2022-06-18 23:16:14 +0530

    Js Edit

--- homepage2/slidebutton.js
+++ homepage2/slidebutton.js
@@ -5,8 +5,14 @@ btn1.addEventListener('click',()=> btn1.style.backgroundColor='blue')
 btn2.addEventListener('click',()=>btn2.style.backgroundColor='white')
 
 function myfun(){
+  
     var x1=document.getElementById('num').value;
-    alert("completed !");
+    if(x1===""){
+        alert("Please Enter Your Number")
+    }
+    else{
+    alert("SMS has been sent to your Mobile.!!");
+    }
 }
 
 
